211b371b2949c5c29b756e69e3377c702a91be71
RohanMagar7 <rmtechyt@gmail.com>
2024-01-21 23:52:33 +0530

    add slider

--- wp-includes/js/min.script.js
+++ wp-includes/js/min.script.js
@@ -11,3 +11,18 @@ fetch('../../../pages/Footer/index.html')
 .then(data=>{
     footer.innerHTML=data;
 })
+
+
+let slideIndex = 0;
+showSlides();
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 6 seconds
+}
