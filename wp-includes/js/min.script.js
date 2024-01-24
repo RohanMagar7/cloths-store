4efc59fa65c64624bf177a17e01fa07ea68da571
Dinesh Shinde <shindedinesh262@gmail.com>
2024-01-24 14:57:28 +0530

    Responsiv nav ,slidere

--- wp-includes/js/min.script.js
+++ wp-includes/js/min.script.js
@@ -1,18 +1,16 @@
 const nav = document.querySelector('.navbar');
 fetch('../../../pages/Header/index.html')
-.then(res => res.text())
-.then(data=>{
-    nav.innerHTML=data;
-})
+  .then(res => res.text())
+  .then(data => {
+    nav.innerHTML = data;
+  })
 
 const footer = document.querySelector('#Footer')
 fetch('../../../pages/Footer/index.html')
-.then(res => res.text())
-.then(data=>{
-    footer.innerHTML=data;
-})
-
-
+  .then(res => res.text())
+  .then(data => {
+    footer.innerHTML = data;
+  })
 let slideIndex = 0;
 showSlides();
 function showSlides() {
@@ -22,8 +20,8 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 6 seconds
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5555); // Change image every 6 seconds
 }
 
