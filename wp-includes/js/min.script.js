a20fcd1bb5aa06c80dd2fda8a0b080a72b75ea52
Dinesh Shinde <shindedinesh262@gmail.com>
2024-01-27 19:04:29 +0530

    Fixed the horizontal scroll behavior and other things

--- wp-includes/js/min.script.js
+++ wp-includes/js/min.script.js
@@ -22,7 +22,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5555); // Change image every 6 seconds
+  setTimeout(showSlides, 3000); // Change image every 6 seconds
 }
 
 
