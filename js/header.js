a70e6072767f5d92004f2e5d7aaa912bbb5fa547
Dinesh Shinde <shindedinesh262@gmail.com>
2024-03-12 16:06:56 +0530

    home nav bar

--- js/header.js
+++ js/header.js
@@ -1,5 +1,33 @@
-let homeMenu= document.querySelector("#menu-bar-icon")
-console.log(homeMenu);
-homeMenu.addEventListener("click",()=>{
-    alert("btn is clicked")
-})
+let homeMenu = document.querySelector("#menu-bar-icon");
+
+homeMenu.addEventListener("click", () => {
+    smallDeviceMenueBar.style.transition = `all 0.5s ease`; // Set transition property first
+    header.insertAdjacentElement("beforebegin", smallDeviceMenueBar);
+
+    // Delay setting the transform property until after the element is added to the DOM
+    setTimeout(() => {
+        smallDeviceMenueBar.style.transform = `translate(${0}px)`;
+    }, 0);
+
+    smallDeviceMenueBar.insertAdjacentElement("afterbegin", navBar);
+    smallDeviceMenueBar.insertAdjacentElement("beforeend", backBtnOfHomeMenue);
+
+    backBtnOfHomeMenue.addEventListener("click", () => {
+        smallDeviceMenueBar.style.transform = `translate(${ -1000}px)`;
+    });
+});
+
+let backBtnOfHomeMenue = document.createElement("i");
+backBtnOfHomeMenue.classList.add("back-btn-of-home-menue", "fa-solid", "fa-angle-down");
+
+let navBar = document.createElement("div");
+navBar.innerHTML = `
+<a her=""><p class="home-menue-bar-item">Home</a></p>
+<a her=""><p class="home-menue-bar-item"></a></p>
+<a her=""><p class="home-menue-bar-item"></a></p>
+`;
+navBar.classList.add("small-device-menue-box");
+
+let smallDeviceMenueBar = document.createElement("div");
+smallDeviceMenueBar.classList.add("small-device-menue");
+  
